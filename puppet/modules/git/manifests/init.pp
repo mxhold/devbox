d7a39f655aedba69bf66baaf6a6110eaaf2212ea
Max Holder <mxhold@gmail.com>
2015-10-24 15:41:26 -0700

    Add gitconfig and gitignore

--- puppet/modules/git/manifests/init.pp
+++ puppet/modules/git/manifests/init.pp
@@ -3,4 +3,20 @@ class git {
     ensure => 'latest',
     require => [Apt::Source['debian_testing']],
   }
+
+  file {
+    '/home/vagrant/.gitconfig':
+      owner => 'vagrant',
+      group => 'vagrant',
+      mode  => '0644',
+      source => 'puppet:///modules/git/gitconfig',
+  }
+
+  file {
+    '/home/vagrant/.gitignore':
+      owner => 'vagrant',
+      group => 'vagrant',
+      mode  => '0644',
+      source => 'puppet:///modules/git/gitignore',
+  }
 }
