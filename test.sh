2399383336018b33fe4de5cecf3e0bb97b6eea7e
Max Holder <mxhold@gmail.com>
2015-10-24 22:46:24 -0700

    Improve test formatting

--- test.sh
+++ test.sh
@@ -9,10 +9,10 @@ function check_program {
   program_name=$1
   expected_version=$2
   version_command=$3
-  actual_version=$(eval $version_command 2>/dev/null)
-  echo -n "checking for $program_name... "
+  actual_version=$(eval $version_command 2>/dev/null | head -1)
+  echo -n "checking $program_name version... "
   if [[ $actual_version == *$expected_version* ]]; then
-    echo "yes"
+    echo "$actual_version"
   else
     echo "no"
     echo "Output from running \"$version_command\" did not match:" >&2
@@ -23,22 +23,23 @@ function check_program {
   fi
 }
 
-check_program "zsh" "zsh 5." "zsh --version"
-check_program "chruby" "chruby: 0." "chruby --version"
-check_program "ruby" "ruby 1.9.3p551" "chruby 1.9.3 && ruby --version"
-check_program "ruby" "ruby 2.0.0p598" "chruby 2.0.0 && ruby --version"
-check_program "ruby" "ruby 2.1.5" "chruby 2.1.5 && ruby --version"
-check_program "ruby" "ruby 2.2.1" "chruby 2.2.1 && ruby --version"
-check_program "ruby-install" "ruby-install: 0." "ruby-install --version"
-check_program "postgres" "\(PostgreSQL\) 9.4" "psql --version"
-check_program "erlang" "\"18\"" "erl -eval 'erlang:display(erlang:system_info(otp_release)), halt().'  -noshell"
-check_program "elixir" "Elixir 1.1" "elixir --version"
-check_program "tmux" "tmux 2." "tmux -V"
-check_program "vim" "Vi IMproved 7.4" "vim --version | head -1"
-check_program "git" "git version 2.6" "git --version"
-check_program "htop" "htop 1." "htop --version"
-check_program "tree" "tree v1." "tree --version"
-check_program "unzip" "UnZip 6." "unzip | head -1"
+#             Program        Expected version     Version command
+check_program "zsh"          "zsh 5."             "zsh --version"
+check_program "chruby"       "chruby: 0."         "chruby --version"
+check_program "ruby"         "ruby 1.9.3p551"     "chruby 1.9.3 && ruby --version"
+check_program "ruby"         "ruby 2.0.0p598"     "chruby 2.0.0 && ruby --version"
+check_program "ruby"         "ruby 2.1.5"         "chruby 2.1.5 && ruby --version"
+check_program "ruby"         "ruby 2.2.1"         "chruby 2.2.1 && ruby --version"
+check_program "ruby-install" "ruby-install: 0."   "ruby-install --version"
+check_program "postgres"     "\(PostgreSQL\) 9.4" "psql --version"
+check_program "erlang"       "\"18\""             "erl -eval 'erlang:display(erlang:system_info(otp_release)), halt().'  -noshell"
+check_program "elixir"       "Elixir 1.1"         "elixir --version"
+check_program "tmux"         "tmux 2."            "tmux -V"
+check_program "vim"          "Vi IMproved 7.4"    "vim --version"
+check_program "git"          "git version 2.6"    "git --version"
+check_program "htop"         "htop 1."            "htop --version"
+check_program "tree"         "tree v1."           "tree --version"
+check_program "unzip"        "UnZip 6."           "unzip"
 
 echo "Success!"
 exit 0
